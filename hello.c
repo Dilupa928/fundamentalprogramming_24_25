4937fc7ddb4bb1ff76d4f52c740cda733db6180c
Dilupa928 <dilupakaushan76@gmail.com>
2026-05-10 12:51:00 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    char name[50];
+    printf("Dilupa: ");
+    scanf("%s",name);
+        printf("Hello %s, welcome to the C Lab!\n", name);
+        return 0;
+}
