296f64efbd6237886a34c5a71c4a5ca890563200
Dilupa928 <dilupakaushan76@gmail.com>
2026-05-10 13:08:03 +0530

    Delete hello.c

--- hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    char name[50];
-    printf("Dilupa: ");
-    scanf("%s",name);
-        printf("Hello %s, welcome to the C Lab!\n", name);
-        return 0;
-}
